6850046e7979bfc5ffd7efaef1d09b31ab340505
Francisco Italo <franciscoitalovianademorais@gmail.com>
2022-11-07 16:03:29 -0300

    Add Class 01 Challenge

--- JavaScript/Study/script.js
+++ JavaScript/Study/script.js
@@ -63,12 +63,73 @@
 
 // >>> Template Strings (interpolation) <<<
 
-let num1 = 20
-let num2 = 25
-let total = num1 + num2
+// let num1 = 20
+// let num2 = 25
+// let total = num1 + num2
 
 // alert("Oi")
-console.log (" A soma de " + num1 + " e " + num2 + " e " + " é igual a  " + total)
+// console.log (" A soma de " + num1 + " e " + num2 + " e " + " é igual a  " + total)
 
-console.log(`A soma de ${num1} e ${num2} é igual a ${total}`)
+// console.log(`A soma de ${num1} e ${num2} é igual a ${total}`)
+
+
+// >>> Type structural (Object, Functions) <<<
+
+// let nome = "Dev"
+// let age = 29
+// let profission = "Web Developer"
+
+// console.log(nome)
+// console.log(age)
+// console.log(profission)
+
+person = {
+
+    nome: "italo",
+    age: 23,
+    profission: "Web Developer" 
+
+}
+
+// console.log(typeof person)
+// console.log(person)
+// console.log(person.nome)
+// console.log(person.age)
+// console.log (`${person.nome} tem ${person.age} anos e é ${person.profission}`)
+
+
+// Array (Vetor)
+
+// countries = ["Brazil", "Germany", "Spain"]
+// countries = [
+//     ["Brazil", 5], ["Germany", 4], ["Spain", 1]
+// ]
+// // console.log(typeof countreis)
+// // console.log (countreis)
+// console.log (countries)
+// console.log (countries[2])
+
+countries = [
+    {
+        name: "Brazil",
+        champions: 5
+    },
+
+    {
+        name: "Germany",
+        champions: 4
+    },
+
+    {
+        name: "Espanha",
+        champions: 1
+    }
+]
+
+let country = 0 
+
+console.log(`${countries[country].name} tem ${countries[country].champions} título(s) mundiais`)
+
+
+
 
