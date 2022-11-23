ced399d054b2036a15bee09a8ff6a3cbca75b718
Francisco Italo <franciscoitalovianademorais@gmail.com>
2022-11-23 15:51:27 -0300

    add Class-06-Loopings

---
+++ JavaScript/class-06-looping/study/script.js
@@ -0,0 +1,29 @@
+// >>>> Loopings <<<<
+
+let count = 11 // flag
+while (count <= 10)
+
+    if (count > 2 && count < 8) {
+    count++ // continue
+    break
+}
+
+if (count % 2 == 0) {
+    console.log(count)
+} 
+count++ // container
+
+// do {
+//     if (count > 2 && count < 8) {
+//         count++ // container
+//         break
+//     }
+    
+//     if (count % 2 == 0) {
+//         console.log(count)
+//     }
+//     count ++ // container 
+// } while (count <= 10)
+
+
+console.log("Minha aplicação")
