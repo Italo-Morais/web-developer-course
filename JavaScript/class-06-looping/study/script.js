f078734518e2ea551729876d5745c5f7a2bf7bb5
Francisco Italo <franciscoitalovianademorais@gmail.com>
2022-11-25 16:14:37 -0300

    class-06-looping

--- JavaScript/class-06-looping/study/script.js
+++ JavaScript/class-06-looping/study/script.js
@@ -1,17 +1,17 @@
 // >>>> Loopings <<<<
 
-let count = 11 // flag
-while (count <= 10)
+// let count = 11 // flag
+// while (count <= 10)
 
-    if (count > 2 && count < 8) {
-    count++ // continue
-    break
-}
+//     if (count > 2 && count < 8) {
+//     count++ // continue
+//     break
+// }
 
-if (count % 2 == 0) {
-    console.log(count)
-} 
-count++ // container
+// if (count % 2 == 0) {
+//     console.log(count)
+// } 
+// count++ // container
 
 // do {
 //     if (count > 2 && count < 8) {
@@ -28,14 +28,46 @@ count++ // container
 
 // console.log("Minha aplicação")
 
-for (let count = 0; count <= 10; count++) {
-    if (count > 2 && count < 8) {
-        // count++ // caution, two increments!
-        continue
-        // break 
-    }
+// for (let count = 0; count <= 10; count++) {
+//     if (count > 2 && count < 8) {
+//         // count++ // caution, two increments!
+//         continue
+//         // break 
+//     }
     
-    if (count % 2 == 0) {
-        console.log(count) 
-    }
-} 
+//     if (count % 2 == 0) {
+//         console.log(count) 
+//     }
+//     count++ //container
+// } 
+
+
+// let resp = true
+// do {
+//     resp = prompt("Digite seu nome: ") 
+// } while (resp)
+
+// >>>> for <<<<
+
+// const array = [1, 2, 4, 7]
+// for (let count = 0; count < array.length; count++) {
+//     const element = array[count]
+// }
+
+// >>>> for of <<<<
+
+// const array = ["A", "B", "C", "D"]
+// for (const iterator of array) {
+//     console.log(iterator) 
+// }
+
+// >>>> for in <<<<
+
+// const array = [1, 2, 3, 4]
+// for (const index in array) {
+//     if (Object.hasOwnProperty.call(array, index)) {
+//         console.log(array[index]);
+//     }
+// }
+
+// >>>> for each <<<<
