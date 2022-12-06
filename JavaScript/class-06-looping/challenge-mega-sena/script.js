5854e6188a75f589e05e89fe4d9264274f29773f
Francisco Italo <franciscoitalovianademorais@gmail.com>
2022-12-06 15:52:35 -0300

    add challenge-mega-sena

--- JavaScript/class-06-looping/challenge-mega-sena/script.js
+++ JavaScript/class-06-looping/challenge-mega-sena/script.js
@@ -7,8 +7,8 @@ const numbersMega = []
 let div = document.querySelector('div')
 
 while(numbersMega.length < 6) {
-    let numberDrawn = String(Math.ceil(Math.random() * 60)
-    numbersMega.push(numberDrawn)).padStart(2, 0)
+    let numberDrawn = String(Math.ceil(Math.random() * 60))
+    numbersMega.push(numberDrawn).padStart(2, 0)
 
     if (!numbersMega.includes(numberDrawn)) {
         numbersMega.push(numberDrawn)
