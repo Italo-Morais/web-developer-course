6d6190944f9366795716b2f2270389c4391cecbc
Francisco Italo <franciscoitalovianademorais@gmail.com>
2022-12-22 15:41:08 -0300

    add class-09-es6-features

--- JavaScript/class-09-es6-features/scripts/script.js
+++ JavaScript/class-09-es6-features/scripts/script.js
@@ -52,6 +52,54 @@ function person(name, age) {
     }
 }
 
-let person1 = person('Emanuel', 30)
+// let person1 = person('Emanuel', 30)
 
-console.log(person1.status())
+// console.log(person1.status())
+
+// >>>> Destructure <<<<
+
+const array = ["A", "B"]
+const [item1, item2] = array
+console.log(array)
+console.log(item1)
+console.log(item2)
+
+// >>> Rest <<<
+
+function showArray(array, ...numbers) {
+    console.log(array, numbers)
+}
+
+showArray(array, 2, 3, 4, 5, 3)
+
+// >>> Spread <<<
+
+const numbers = [2, 3, 4, 5]
+// const clone = [...numbers]
+const [...clone] = numbers
+clone.pop()
+
+console.log(Math.max(...numbers))
+console.log(...numbers)
+console.log(clone)
+
+const list = document.querySelectorAll('div')
+console.log(list)
+
+list.forEach((value) => console.log(value));
+
+[...list].map((value) => console.log(value))
+
+let num;num = 7
+console.log(num)
+
+// ASI (Automatic Semicolon Insertion)
+
+// Babel (ES6 to ES5)
+// - Parser: Code to AST (Abstract Syntex Tree)
+// - Transformer: AST Manipulates (JSON)
+// - Generator: AST to Code 5
+
+
+
+    
