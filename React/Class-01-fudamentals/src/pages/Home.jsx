efd93c776512549c544488b3009ace7e940af217
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-01-02 16:47:09 -0300

    Class-React-Fundamentals

---
+++ React/Class-01-fudamentals/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+export function Home() {
+
+  return (
+    <div>
+    <header>
+      <h1>Class React - Fundamentals</h1>
+    </header>
+    <main>
+      <article>
+        <section>
+          <h2>Paragraph 1</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum optio facilis saepe aspernatur. Impedit animi architecto tempore doloribus aliquid quae voluptate ipsum quos earum? Porro molestias atque iure voluptas perferendis?</p>
+        </section>
+        <section>
+          <h2>Paragraph 2</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam rem, autem fugiat asperiores debitis ea vero modi. Aliquid optio similique maxime enim nemo recusandae aperiam molestias dolore nesciunt, cupiditate maiores?</p>
+        </section>
+      </article>
+    </main>
+    </div>
+    
+  )
+}
+
+
