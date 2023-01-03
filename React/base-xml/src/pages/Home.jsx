c7cf93f9719a50bdb759f91cfa40f898e2efc4e8
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-01-03 16:24:00 -0300

    Class React - Base XML

---
+++ React/base-xml/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import ReactImage from "../images/react.svg"
+
+import "./Home.css"  
+
+export function Home() {
+
+  return (
+    <div className="container">
+      <h1>Class React - Base XML</h1>
+      <img src={ReactImage} alt="" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis vel hic vitae, aliquid quisquam modi. Explicabo atque officiis temporibus! Maiores sint, repellendus cum unde obcaecati repellat dolorem labore veniam id.
+      </p>
+    </div>
+  )
+}
+
+
