5967fa9bc9dcea76b3a1f50ea98693235ae9ed64
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-01-05 16:29:30 -0300

    add class-03-hooks

---
+++ React/class-03-hooks/src/pages/App.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+import './App.css'
+
+export function App() {
+  const [name, setName] = useState('Emanuel');
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('')
+  const [text2, setText2] = useState('')
+  // let count = 0;
+  // console.log(name);
+  return (
+    <div className="container">
+      {/* <h1>Class Hooks</h1>
+      <p>Nome: {name}</p>
+      <p>Number: {count}</p>
+      <button onClick={() => {
+        setCount(count + 1)
+        console.log(count);
+      }}>Contar</button> */}
+
+      <form action="">
+        <p>Palavra: {text}</p>
+        <p>Palavra: {text2}</p>
+        <input type="text" onChange={() => setText(event.target.value)}/>
+        <button onClick={(event) => {event.preventDefault()
+        setText2(text)}}>Enviar</button>
+      </form>
+    </div>
+  )
+}
+
