9817127ac375226cba18022d4f2d6e26a72e5920
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-01-09 16:39:24 -0300

    add class-04-routes

---
+++ React/class-04-routes/class-04-routes/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import reactLogo from '../../assets/react.svg'
+import './style.css'
+
+export function Home() {
+
+  return (
+    <div className="container">
+      <h1>Home</h1>
+      <img src={reactLogo} alt="" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsam illo consequatur dolores! Aut quo tenetur laborum impedit, dolore ipsa quod. Repellat eos totam, repellendus dolore porro maiores minima dignissimos.</p>
+    </div>
+  )
+}
+
