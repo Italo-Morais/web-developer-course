f0966f52e51ff99ee2c7ef9bc528e5bab9b1764a
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-03-01 16:38:20 -0300

    add jwt

--- Node.JS/class-07-authentication/src/controllers/auth.js
+++ Node.JS/class-07-authentication/src/controllers/auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const prisma = require('../database');
+const jwt = require('jsonwebtoken');
+const { secret, expiresIn } = require('../configs/auth.json');
 
 
 module.exports = {
@@ -9,11 +11,19 @@ module.exports = {
     if (!password) return res.status(400).json("Password is required");
   
     const user = await prisma.users.findFirst({ where: { email } });
-      if (!user) return res.status(400).json("User not found");
+      if (!user) return res.status(400).json("Email or Password is incorrect");
 
     const passwordCheck = await bcrypt.compare(password, user.password);
-    if (!passwordCheck) return res.status(400).json("Password is incorrect");
+    if (!passwordCheck) return res.status(400).json("Email or Password is incorrect");
 
+    const token = jwt.sign({ id: user.id }, secret, {expiresIn});
+    return res.status(200).json({
+      id: user.id,
+      status: "sucess", 
+      token: token
+       });
+
+    
     return res.status(200).json({status: "success"});
 
     
