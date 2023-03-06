a79d265fbb3c53c37a1288d96bfedd8c0092406c
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-03-06 16:48:46 -0300

    add project students-system part1

---
+++ Projects/students-system/client/src/App/index.jsx
@@ -0,0 +1,12 @@
+import { Home } from '../pages/Home'
+import { Container } from './style'
+
+export function App() {
+  return (
+     <div className="App">
+    <Home>
+      <h1>OlÁ mUNDO !</h1>
+    </Home>
+    </div>
+  )
+}
