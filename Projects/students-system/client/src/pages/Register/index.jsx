4cac28b4f4249b55694d51bebe28efe8face58be
Italo Morais <franciscoitalovianademorais@gmail.com>
2023-03-09 05:25:41 -0300

    add form in register

--- Projects/students-system/client/src/pages/Register/index.jsx
+++ Projects/students-system/client/src/pages/Register/index.jsx
@@ -1,11 +1,62 @@
-import { Container } from './styled' 
+import { Container } from "./styled";
+import { useState } from "react";
 
 export function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignUpForm = (form) => {
+    event.preventDefault();
+    console.log(handleSignUpForm);
+  };
+
   return (
-     <div className="Home">
     <Container>
-      <h1>Register</h1>
+      <div className="container">
+        <h1>Register</h1>
+        <form
+          className="form"
+          action="Register"
+          id="register"
+          onSubmit={handleSignUpForm}
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            required
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            required
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </Container>
-    </div>
-  )
+  );
 }
--- Projects/students-system/client/src/pages/Register/styled.js
+++ Projects/students-system/client/src/pages/Register/styled.js
@@ -1,7 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1.8rem;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    max-width: 40.0rem;
+    gap: 1rem;
+
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+
+  }
   h1 {
-    color: ${({theme}) => theme.colors.neutral9};
+    color: ${({ theme }) => theme.colors.neutral9};
+    text-align: center;
+    margin-top: 1.8rem;
   }
 `;
