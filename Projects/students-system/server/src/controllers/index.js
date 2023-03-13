3131a6bbf6b9bdb6bf90d220174064fe4bb9f3b4
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-03-13 16:44:52 -0300

    add server in project

---
+++ Projects/students-system/server/src/controllers/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  async read(req, res) {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async create(req, res) {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async update(req, res) {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async delete(req, res) {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
---
+++ Projects/students-system/server/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, console.log('Server is Running'));
+
+app.use(express.json());
+
+const routes = require('./routes');
+app.use(routes);
+
+const prisma = require('./database');
+prisma.$connect().then(() => console.log('Database is running'))
+.catch((error) => console.log(error));
