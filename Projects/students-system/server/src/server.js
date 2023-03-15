e96c1538e94efc8a4adccbd046ac04a325c5779c
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-03-15 17:01:45 -0300

    add cors in server

--- Projects/students-system/server/src/server.js
+++ Projects/students-system/server/src/server.js
@@ -6,6 +6,9 @@ app.listen(port, console.log('Server is Running'));
 
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors());
+
 const routes = require('./routes');
 app.use(routes);
 
