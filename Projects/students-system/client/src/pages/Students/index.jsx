e24bffdb00bcacef8d7ea2fcc19e7bb9b981b5c8
Francisco Italo <franciscoitalovianademorais@gmail.com>
2023-03-15 17:02:36 -0300

    add HandleStudents component

--- Projects/students-system/client/src/pages/Students/index.jsx
+++ Projects/students-system/client/src/pages/Students/index.jsx
@@ -1,10 +1,11 @@
+import { HandleStudents } from '../../components/HandleStudents'
 import { Container } from './styled' 
 
 export function Students() {
   return (
      <div className="Home">
     <Container>
-      <h1>Students</h1>
+      <HandleStudents />
     </Container>
     </div>
   )
