8685d8493413b6522fbbaad33be92d8fb630361e
Italo Morais <franciscoitalovianademorais@gmail.com>
2023-03-18 08:56:28 -0300

    fix css

--- Projects/students-system/client/src/pages/Home/styled.js
+++ Projects/students-system/client/src/pages/Home/styled.js
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 
-
-import styled from "styled-components";
-
 export const Container = styled.div `
     h1 {
         color: ${({theme}) => theme.colors.main3};
