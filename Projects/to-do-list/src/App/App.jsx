6c6de6f0320d06b99cb1ed14e1f865825ea04696
Italo Morais <franciscoitalovianademorais@gmail.com>
2023-03-19 22:10:52 -0300

    add input and button

--- Projects/to-do-list/src/App/App.jsx
+++ Projects/to-do-list/src/App/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import { IoMdRocket } from "react-icons/io";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 export function App() {
   return (
@@ -9,6 +10,19 @@ export function App() {
         <h1 className={styles.to}>to</h1>
         <h1 className={styles.do}>do</h1>
       </header>
+      <form className={styles.form} action="submit">
+        <input
+          onClick={styles.onClick}
+          className={styles.formInput}
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+        />
+        <button className={styles.formButton} type="submit">
+          <span className={styles.buttonText}>
+            Criar <AiOutlinePlusCircle />
+          </span>
+        </button>
+      </form>
     </div>
   );
 }
